1cbcd79533ea5b113c69e6fc3a1c840f5b7d9037
Matt Jones <gitcode@magisa.org>
2023-05-15 21:57:00 -0800

    Reformat Payment class to follow style standards.

--- src/main/java/org/dataone/bookkeeper/api/Payment.java
+++ src/main/java/org/dataone/bookkeeper/api/Payment.java
@@ -1,21 +1,19 @@
 /*
- * This work was created by participants in the DataONE project, and is
- * jointly copyrighted by participating institutions in DataONE. For
- * more information on DataONE, see our web site at http://dataone.org.
+ * This work was created by participants in the DataONE project, and is jointly copyrighted by
+ * participating institutions in DataONE. For more information on DataONE, see our web site at
+ * http://dataone.org.
  *
- *   Copyright 2019. All rights reserved.
+ * Copyright 2019. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
  *
  */
 
@@ -24,26 +22,21 @@ package org.dataone.bookkeeper.api;
 import java.util.ArrayList;
 import java.util.LinkedHashMap;
 import java.util.Map;
-
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 
-
 /**
  * 
- * Payment represents the payment information received from 
- * a payment processor. Successful receipt of payment information
- * indicates whether a payment was approved or not, and thus whether
- * an Order can transition from created to paid state.
+ * Payment represents the payment information received from a payment processor. Successful receipt
+ * of payment information indicates whether a payment was approved or not, and thus whether an Order
+ * can transition from created to paid state.
  */
 @JsonIgnoreProperties({"log"})
 @JsonInclude(JsonInclude.Include.NON_NULL)
@@ -52,8 +45,8 @@ public class Payment {
 
     private final Log log = LogFactory.getLog(Payment.class);
 
-    @NotEmpty 
-    @NotNull 
+    @NotEmpty
+    @NotNull
     private String accountId;
     private String timestamp;
     private String count;
@@ -102,8 +95,7 @@ public class Payment {
      * No args constructor for use in serialization
      *
      */
-    public Payment() {
-    }
+    public Payment() {}
 
     /**
      *
@@ -145,33 +137,31 @@ public class Payment {
      * @param transactionApproved
      * @param hiddenRecaptcha
      */
-    public Payment(
-            @NotEmpty @NotNull String accountId,
-            @NotEmpty @NotNull String timestamp,
-            @NotEmpty @NotNull String count,
-            @NotEmpty @NotNull String hash,
-            String transactionId,
-            // String tenderType, 
-            String transactionTimestamp, 
+    public Payment(@NotEmpty @NotNull String accountId, @NotEmpty @NotNull String timestamp,
+            @NotEmpty @NotNull String count, @NotEmpty @NotNull String hash, String transactionId,
+            // String tenderType,
+            String transactionTimestamp,
             // String cardBrand,
-            // String transactionType, String last4, String cardExpiration, 
-            String authorizationCode, String authorizationMessage, 
-            String requestAmount, String transactionAmount,
+            // String transactionType, String last4, String cardExpiration,
+            String authorizationCode, String authorizationMessage, String requestAmount,
+            String transactionAmount,
             // String fid, String ipAddress,
             // String firstName, String lastName, String keyed, String swiped, String entryMode,
             String transactionApproved,
             // String avsResponse, String reasonCode, String traceNumber, String network,
-            // String currency, String error, String errorCode, String errorMessage, String errorMsg, 
+            // String currency, String error, String errorCode, String errorMessage, String
+            // errorMsg,
             String orderId, String products
-            // String firstLastName, String gRecaptchaResponse, String hiddenRecaptcha,
-            // String hostedPaymentSignal, String callbackMethod, String expressCheckOutPayload, String description
-            ) {
+    // String firstLastName, String gRecaptchaResponse, String hiddenRecaptcha,
+    // String hostedPaymentSignal, String callbackMethod, String expressCheckOutPayload, String
+    // description
+    ) {
         super();
         this.accountId = accountId;
         this.timestamp = timestamp;
         this.count = count;
         this.hash = hash;
-    
+
         this.transactionId = transactionId;
         // this.tenderType = tenderType;
         this.transactionTimestamp = transactionTimestamp;
@@ -215,7 +205,7 @@ public class Payment {
     public String getAccountId() {
         return accountId;
     }
-    
+
     @JsonProperty
     public void setAccountId(String accountId) {
         this.accountId = accountId;
@@ -254,16 +244,16 @@ public class Payment {
     @SuppressWarnings("unchecked")
     @JsonProperty("responses")
     private void unpackFirstElement(ArrayList<Object> responses) {
-        Map<String, String> firstResponse = (Map<String, String>)responses.get(0);
-        this.transactionId = (String)firstResponse.get("transaction_id");
-        this.authorizationCode = (String)firstResponse.get("authorization_code");
-        this.authorizationMessage = (String)firstResponse.get("authorization_message");
-        this.requestAmount = (String)firstResponse.get("request_amount");
-        this.transactionAmount = (String)firstResponse.get("transaction_amount");
-        this.orderId = (String)firstResponse.get("orderid");
-        this.products = (String)firstResponse.get("products");
-        this.transactionApproved = (String)firstResponse.get("transaction_approved");
-        this.transactionTimestamp = (String)firstResponse.get("transaction_timestamp");
+        Map<String, String> firstResponse = (Map<String, String>) responses.get(0);
+        this.transactionId = (String) firstResponse.get("transaction_id");
+        this.authorizationCode = (String) firstResponse.get("authorization_code");
+        this.authorizationMessage = (String) firstResponse.get("authorization_message");
+        this.requestAmount = (String) firstResponse.get("request_amount");
+        this.transactionAmount = (String) firstResponse.get("transaction_amount");
+        this.orderId = (String) firstResponse.get("orderid");
+        this.products = (String) firstResponse.get("products");
+        this.transactionApproved = (String) firstResponse.get("transaction_approved");
+        this.transactionTimestamp = (String) firstResponse.get("transaction_timestamp");
         log.debug(this.transactionAmount);
     }
 
@@ -278,11 +268,11 @@ public class Payment {
     }
 
     // public String getTenderType() {
-    //     return tenderType;
+    // return tenderType;
     // }
 
     // public void setTenderType(String tenderType) {
-    //     this.tenderType = tenderType;
+    // this.tenderType = tenderType;
     // }
 
     @JsonProperty
@@ -296,35 +286,35 @@ public class Payment {
     }
 
     // public String getCardBrand() {
-    //     return cardBrand;
+    // return cardBrand;
     // }
 
     // public void setCardBrand(String cardBrand) {
-    //     this.cardBrand = cardBrand;
+    // this.cardBrand = cardBrand;
     // }
 
     // public String getTransactionType() {
-    //     return transactionType;
+    // return transactionType;
     // }
 
     // public void setTransactionType(String transactionType) {
-    //     this.transactionType = transactionType;
+    // this.transactionType = transactionType;
     // }
 
     // public String getLast4() {
-    //     return last4;
+    // return last4;
     // }
 
     // public void setLast4(String last4) {
-    //     this.last4 = last4;
+    // this.last4 = last4;
     // }
 
     // public String getCardExpiration() {
-    //     return cardExpiration;
+    // return cardExpiration;
     // }
 
     // public void setCardExpiration(String cardExpiration) {
-    //     this.cardExpiration = cardExpiration;
+    // this.cardExpiration = cardExpiration;
     // }
 
     @JsonProperty
@@ -368,59 +358,59 @@ public class Payment {
     }
 
     // public String getFid() {
-    //     return fid;
+    // return fid;
     // }
 
     // public void setFid(String fid) {
-    //     this.fid = fid;
+    // this.fid = fid;
     // }
 
     // public String getIpAddress() {
-    //     return ipAddress;
+    // return ipAddress;
     // }
 
     // public void setIpAddress(String ipAddress) {
-    //     this.ipAddress = ipAddress;
+    // this.ipAddress = ipAddress;
     // }
 
     // public String getFirstName() {
-    //     return firstName;
+    // return firstName;
     // }
 
     // public void setFirstName(String firstName) {
-    //     this.firstName = firstName;
+    // this.firstName = firstName;
     // }
 
     // public String getLastName() {
-    //     return lastName;
+    // return lastName;
     // }
 
     // public void setLastName(String lastName) {
-    //     this.lastName = lastName;
+    // this.lastName = lastName;
     // }
 
     // public String getKeyed() {
-    //     return keyed;
+    // return keyed;
     // }
 
     // public void setKeyed(String keyed) {
-    //     this.keyed = keyed;
+    // this.keyed = keyed;
     // }
 
     // public String getSwiped() {
-    //     return swiped;
+    // return swiped;
     // }
 
     // public void setSwiped(String swiped) {
-    //     this.swiped = swiped;
+    // this.swiped = swiped;
     // }
 
     // public String getEntryMode() {
-    //     return entryMode;
+    // return entryMode;
     // }
 
     // public void setEntryMode(String entryMode) {
-    //     this.entryMode = entryMode;
+    // this.entryMode = entryMode;
     // }
 
     @JsonProperty
@@ -434,75 +424,75 @@ public class Payment {
     }
 
     // public String getAvsResponse() {
-    //     return avsResponse;
+    // return avsResponse;
     // }
 
     // public void setAvsResponse(String avsResponse) {
-    //     this.avsResponse = avsResponse;
+    // this.avsResponse = avsResponse;
     // }
 
     // public String getReasonCode() {
-    //     return reasonCode;
+    // return reasonCode;
     // }
 
     // public void setReasonCode(String reasonCode) {
-    //     this.reasonCode = reasonCode;
+    // this.reasonCode = reasonCode;
     // }
 
     // public String getTraceNumber() {
-    //     return traceNumber;
+    // return traceNumber;
     // }
 
     // public void setTraceNumber(String traceNumber) {
-    //     this.traceNumber = traceNumber;
+    // this.traceNumber = traceNumber;
     // }
 
     // public String getNetwork() {
-    //     return network;
+    // return network;
     // }
 
     // public void setNetwork(String network) {
-    //     this.network = network;
+    // this.network = network;
     // }
 
     // public String getCurrency() {
-    //     return currency;
+    // return currency;
     // }
 
     // public void setCurrency(String currency) {
-    //     this.currency = currency;
+    // this.currency = currency;
     // }
 
     // public String getError() {
-    //     return error;
+    // return error;
     // }
 
     // public void setError(String error) {
-    //     this.error = error;
+    // this.error = error;
     // }
 
     // public String getErrorCode() {
-    //     return errorCode;
+    // return errorCode;
     // }
 
     // public void setErrorCode(String errorCode) {
-    //     this.errorCode = errorCode;
+    // this.errorCode = errorCode;
     // }
 
     // public String getErrorMessage() {
-    //     return errorMessage;
+    // return errorMessage;
     // }
 
     // public void setErrorMessage(String errorMessage) {
-    //     this.errorMessage = errorMessage;
+    // this.errorMessage = errorMessage;
     // }
 
     // public String getErrorMsg() {
-    //     return errorMsg;
+    // return errorMsg;
     // }
 
     // public void setErrorMsg(String errorMsg) {
-    //     this.errorMsg = errorMsg;
+    // this.errorMsg = errorMsg;
     // }
 
     @JsonProperty
@@ -526,59 +516,59 @@ public class Payment {
     }
 
     // public String getFirstLastName() {
-    //     return firstLastName;
+    // return firstLastName;
     // }
 
     // public void setFirstLastName(String firstLastName) {
-    //     this.firstLastName = firstLastName;
+    // this.firstLastName = firstLastName;
     // }
 
     // public String getgRecaptchaResponse() {
-    //     return gRecaptchaResponse;
+    // return gRecaptchaResponse;
     // }
 
     // public void setgRecaptchaResponse(String gRecaptchaResponse) {
-    //     this.gRecaptchaResponse = gRecaptchaResponse;
+    // this.gRecaptchaResponse = gRecaptchaResponse;
     // }
 
     // public String getHiddenRecaptcha() {
-    //     return hiddenRecaptcha;
+    // return hiddenRecaptcha;
     // }
 
     // public void setHiddenRecaptcha(String hiddenRecaptcha) {
-    //     this.hiddenRecaptcha = hiddenRecaptcha;
+    // this.hiddenRecaptcha = hiddenRecaptcha;
     // }
 
     // public String getHostedPaymentSignal() {
-    //     return hostedPaymentSignal;
+    // return hostedPaymentSignal;
     // }
 
     // public void setHostedPaymentSignal(String hostedPaymentSignal) {
-    //     this.hostedPaymentSignal = hostedPaymentSignal;
+    // this.hostedPaymentSignal = hostedPaymentSignal;
     // }
 
     // public String getCallbackMethod() {
-    //     return callbackMethod;
+    // return callbackMethod;
     // }
 
     // public void setCallbackMethod(String callbackMethod) {
-    //     this.callbackMethod = callbackMethod;
+    // this.callbackMethod = callbackMethod;
     // }
 
     // public String getExpressCheckOutPayload() {
-    //     return expressCheckOutPayload;
+    // return expressCheckOutPayload;
     // }
 
     // public void setExpressCheckOutPayload(String expressCheckOutPayload) {
-    //     this.expressCheckOutPayload = expressCheckOutPayload;
+    // this.expressCheckOutPayload = expressCheckOutPayload;
     // }
 
     // public String getDescription() {
-    //     return description;
+    // return description;
     // }
 
     // public void setDescription(String description) {
-    //     this.description = description;
+    // this.description = description;
     // }
 
     public Map<String, Object> getAdditionalProperties() {
